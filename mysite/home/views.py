8239e783d168ef05fa421d038b3cf0a30963e9e9
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-04-01 15:40:53 +0545

    template view

--- mysite/home/views.py
+++ mysite/home/views.py
@@ -1,6 +1,13 @@
 from django.shortcuts import render
-
+from django.views.generic import TemplateView,CreateView
 # Create your views here.
 
-def index(request):
-    return render(request,'home_folder/index.html')
+# def index(request):
+#     return render(request,'home_folder/index.html')
+
+class IndexView(TemplateView):
+    template_name= 'home_folder/index.html'
+
+
+
+
