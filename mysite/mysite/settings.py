8be6ed4bcece35f2d51adf03ab475cbd69a01f1f
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-04-01 17:43:03 +0545

    changes

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -14,6 +14,8 @@ from pathlib import Path
 import os
 from dotenv import load_dotenv
 load_dotenv()
+from django.conf import settings 
+from django.conf.urls.static import static
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -120,10 +122,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'home/static'),
+STATIC_URL = "static/"
+STATICFILES_DIRS = [
+    BASE_DIR / "static"
 ]
-STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -22,4 +22,5 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/',include("home.urls")),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
