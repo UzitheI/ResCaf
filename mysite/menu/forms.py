e67db7dca515453805a5721d7bf4ed930dbbf460
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-04-10 19:40:44 +0545

    search-form

---
+++ mysite/menu/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from . models import Dish
+
+
+class DishForm(forms.ModelForm):
+    class Meta:
+        model=Dish 
+        fields='__all__'
+
+    def __init__(self,*args,**kwargs):
+        super(DishForm,self).__init__(*args,**kwargs)
+        self.fields['country_of_origin'].empty_label="SELECT"
+
+class searchForm(forms.Form): 
+    query=forms.CharField(label='Search')
+
+
+    
