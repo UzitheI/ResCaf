7dfc3e6168957bd9755815c5e4edd238537fa44c
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-04-13 00:24:27 +0545

    added chefdetails, reviews, blog

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -10,6 +10,26 @@ class BookATable(models.Model):
     numberOfPeople=models.IntegerField()
     dateOfBooking=models.DateTimeField()
 
+class ChefDetails(models.Model):
+    name=models.CharField(max_length=244)
+    position=models.CharField(max_length=244)
+    description=models.CharField(max_length=244)
+    image=models.CharField(max_length=244)
+    facebook=models.CharField(max_length=244)
+    twitter=models.CharField(max_length=244)
+    pinterest=models.CharField(max_length=244)
+    linkedin=models.CharField(max_length=244)
+    
+# class Reviews(models.Model):
+#     rating=models.FloatField()
+#     title=models.CharField(max_length=244)
+#     Description=models.CharField(max_length=244)
 
 
+class BlogDetails(models.Model):
+    title=models.CharField(max_length=224)
+    description=models.CharField(max_length=224)
+    header_image=models.CharField(max_length=224)
+    writer=models.CharField(max_length=224)
+    date=models.DateField()
 
