7f3d3320e3c79d48500965a092f0019df51b3885
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-04-19 09:27:48 +0545

    Added Blog, Review and Reservation urls

--- mysite/home/urls.py
+++ mysite/home/urls.py
@@ -3,11 +3,16 @@ from . import views
 
 
 urlpatterns = [
-    path('book',views.BookingView.as_view(),name='book'),
     path('reservation',views.ReservationView.as_view(),name='reservation'),
     path('',views.IndexView.as_view(), name="index"),
-    path('table/<int:pk>/',views.TableUpdatView.as_view(),name='table_update'),
-
+    path('reservationUpdate/<int:pk>/',views.ReservationUpdate.as_view(),name='reservationUpdate'),
+    path('reservationDelete/<int:pk>/',views.ReservationDelete.as_view(),name='reservationDelete'),
+    path('review',views.ReviewView.as_view(),name="review"),
+    path('reviewUpdate/<int:pk>/',views.ReviewUpdateView.as_view(),name="review_update"),
+    path('reviewDelete/<int:pk>/',views.ReviewDelete.as_view(),name="review_delete"),
+    path('blog',views.BlogView.as_view(), name="blog"),
+    path('blogUpdate/<int:pk>/',views.BlogUpdateView.as_view(),name="blog_update"),
+    path('blogDelete/<int:pk>/',views.BlogDeleteView.as_view(),name="blog_delete")
 ]
 
 
