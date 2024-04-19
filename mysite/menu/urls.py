c51436ceab4a45b1044bd8d105b1cf986f1e23bc
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-04-19 09:30:04 +0545

    menu:urls

--- mysite/menu/urls.py
+++ mysite/menu/urls.py
@@ -1,15 +1,16 @@
 from django.urls import reverse_lazy, include, path
-from . views import RemoveFromCart,MenuView, AddDish, UpdateDish, DeleteDish, AddToCartView, CartView, ErrorView
+from . views import RemoveFromCart,MenuView, AddDish, UpdateDish, DeleteDish, AddToCartView, QuantityUpdateView, CartView, ErrorView
 from . import views
 
 app_name = 'menu'
 urlpatterns = [
     path('createDish',AddDish.as_view(),name='createDish'),
-    path('menu',MenuView.as_view(),name='menu'),
+    path('menuList',MenuView.as_view(),name='menuList'),
     path('updateDish/<int:pk>/', UpdateDish.as_view(), name='updateDish'),
     path('deleteDish/<int:pk>/', DeleteDish.as_view(), name='deleteDish'),
     path('addtoCart/<int:dish_id>/', AddToCartView.as_view(), name="addtoCart"),
-    path('remove/<int:dish_id>/', RemoveFromCart.as_view(), name='remove_from_cart'),
+    path('remove/<int:pk>/', RemoveFromCart.as_view(), name='remove_from_cart'),
+    path('quantityUpdate/<int:item_id>/', QuantityUpdateView.as_view(), name='quantityUpdate'),
     path('view_cart', CartView.as_view(), name='view_cart'),
     path('error', ErrorView.as_view(), name="error")
 ]
