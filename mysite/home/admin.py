0be601ae93b1ad00da7899ffff264331f3034518
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-04-19 09:25:09 +0545

    added bookatable

--- mysite/home/admin.py
+++ mysite/home/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
-
+from home.views import ChefDetails, BlogDetails,BookATable, Table, Review
 # Register your models here.
+admin.site.register(ChefDetails)
+admin.site.register(BlogDetails)
+admin.site.register(BookATable)
+admin.site.register(Table)
+admin.site.register(Review)
