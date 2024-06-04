0bd146e7b7b542c832c8045051b0d293cf6e0881
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-06-04 23:50:03 +0545

    removed view all cart items for seperate dispatch

--- mysite/menu/urls.py
+++ mysite/menu/urls.py
@@ -1,19 +1,19 @@
 from django.urls import reverse_lazy, include, path
-from . views import RemoveFromCart,MenuView, AddDish, UpdateDish, DeleteDish, AddToCartView, QuantityUpdateView, CartView, ErrorView, AddSuggestions, SuggestionUpdateView, SuggestionDeleteView
 from . import views
 
 app_name = 'menu'
 urlpatterns = [
-    path('createDish',AddDish.as_view(),name='createDish'),
-    path('menuList',MenuView.as_view(),name='menuList'),
-    path('updateDish/<int:pk>/', UpdateDish.as_view(), name='updateDish'),
-    path('deleteDish/<int:pk>/', DeleteDish.as_view(), name='deleteDish'),
-    path('addtoCart/<int:dish_id>/', AddToCartView.as_view(), name="addtoCart"),
-    path('remove/<int:pk>/', RemoveFromCart.as_view(), name='remove_from_cart'),
-    path('quantityUpdate/<int:item_id>/', QuantityUpdateView.as_view(), name='quantityUpdate'),
-    path('view_cart', CartView.as_view(), name='view_cart'),
-    path('error', ErrorView.as_view(), name="error"),
-    path('suggestions',AddSuggestions.as_view(), name="addsuggestions"),
-    path('suggUpdate/<int:pk>/',SuggestionUpdateView.as_view(),name="sugg_update"),
-    path('suggDelete/<int:pk>/',SuggestionDeleteView.as_view(),name="sugg_delete")
+    path('createDish',views.AddDish.as_view(),name='createDish'),
+    path('menuList',views.MenuView.as_view(),name='menuList'),
+    path('updateDish/<int:pk>/',views.UpdateDish.as_view(), name='updateDish'),
+    path('deleteDish/<int:pk>/', views.DeleteDish.as_view(), name='deleteDish'),
+    path('addtoCart/<int:dish_id>/', views.AddToCartView.as_view(), name="addtoCart"),
+    path('remove/<int:pk>/', views.RemoveFromCart.as_view(), name='remove_from_cart'),
+    path('quantityUpdate/<int:item_id>/', views.QuantityUpdateView.as_view(), name='quantityUpdate'),
+    path('view_cart', views.CartView.as_view(), name='view_cart'),
+    path('error', views.ErrorView.as_view(), name="error"),
+    path('suggestions',views.AddSuggestions.as_view(), name="addsuggestions"),
+    path('suggUpdate/<int:pk>/',views.SuggestionUpdateView.as_view(),name="sugg_update"),
+    path('suggDelete/<int:pk>/',views.SuggestionDeleteView.as_view(),name="sugg_delete"),
+    # path('viewAllOrders',views.ViewAllCartItems.as_view(),name="viewAllCartItems"),
 ]
