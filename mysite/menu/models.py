0d22a8dd6a482729d75cb4f74bc776c6177f797c
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-06-06 23:26:39 +0545

    tweaks

--- mysite/menu/models.py
+++ mysite/menu/models.py
@@ -25,9 +25,9 @@ class CartItem(models.Model):
     dish=models.ForeignKey('Dish',on_delete=models.CASCADE)
     quantity=models.PositiveIntegerField(default=0)
     date_added=models.DateTimeField(auto_now_add=True, null=True)
-
-    def __str__(self):
-        return f'{self.quantity} x {self.dish.name}'
+    total_amount = models.FloatField(default=0.0)
+    message=models.CharField(max_length=200 ,default="Please Wait! We are processing your Order")
+    
 
 class UserSuggestions(models.Model):
     name= models.CharField(max_length=200)
