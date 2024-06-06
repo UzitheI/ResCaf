2ed02c566229a01a901245f79c7bc49c834b8ad5
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-06-06 23:24:36 +0545

    message_form

--- mysite/customadmin/forms.py
+++ mysite/customadmin/forms.py
@@ -1,10 +1,22 @@
 from django import forms 
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm
+from menu.models import CartItem
 
 class UserRegistrationForm( UserCreationForm):
     email=forms.EmailField()
 
     class Meta:
         model=User 
-        fields=['username','email','first_name','last_name']
+        fields= '__all__'
+
+
+class UpdateMessageForm(forms.Form):
+    message= forms.CharField( max_length=200, required=True, label="Enter your message:")
+
+    class Meta:
+        model= CartItem
+        fields = ['message']
+
+
+
