b5a7eae67d2966eb595596aac2fb95d8e9968bc2
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-06-06 23:25:35 +0545

    Update Message View

--- mysite/customadmin/views.py
+++ mysite/customadmin/views.py
@@ -1,11 +1,14 @@
-from django.http import HttpRequest
+
 from django.http.response import HttpResponse as HttpResponse
 from django.shortcuts import render
+from django.urls import reverse 
 from django.contrib.messages.views import SuccessMessageMixin
 from django.views.generic import *
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from customadmin.forms import UserRegistrationForm
+from menu.models import CartItem
+from customadmin.forms import *
 
 # Create your views here.
 class RegisterView(SuccessMessageMixin, CreateView):
@@ -26,8 +29,33 @@ class LoginLogic(LoginView):
             return reverse_lazy('dashboard')
         return reverse_lazy('index')
 
-class DashBoardView(TemplateView):
+class DashBoardView(ListView):
+    model= CartItem
     template_name="home_folder/dashboard_index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["all_orders"] = CartItem.objects.all()
+        return context
+        
+class CartItemDB(ListView):
+    model= CartItem
+    template_name="home_folder/cartitems_db.html"
+
+    def get_context_data(self, **kwargs):
+        context= super().get_context_data(**kwargs)
+        context["all_orders"]=CartItem.objects.all()
+        return context
+
+class UpdateMessage(UpdateView):
+    model= CartItem
+    form_class= UpdateMessageForm
+    template_name= "home_folder/cartitems_db.html"
+    success_url= reverse_lazy('cartitems_dashboard')
+
+
+
+
 
 
     
