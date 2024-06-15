7a58ef26c882a68c607c5faadeb697491d93e542
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-06-16 00:15:07 +0545

    dashboard views final

--- mysite/customadmin/views.py
+++ mysite/customadmin/views.py
@@ -8,6 +8,7 @@ from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
 from customadmin.forms import UserRegistrationForm
 from menu.models import *
+from home.models import *
 from customadmin.forms import *
 from django.shortcuts import get_object_or_404
 # Create your views here.
@@ -35,6 +36,10 @@ class DashBoardView(ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context["all_orders"] = CartItem.objects.all()
+        if self.request.user.is_authenticated:
+            context["user"]=self.request.user
+        else:
+            context["user"]="Guest"
         return context
     
         
@@ -70,7 +75,69 @@ class UpdateMessage(UpdateView):
         print(context['cart_item'].pk)
         return context
 
+class ReviewDB(ListView):
+    model= Review
+    template_name= 'home_folder/review_db.html'
 
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        context['reviews']= Review.objects.all()
+        context['form']=UpdateReviewForm
+        return context
+
+class ReviewUpdateDB(UpdateView):
+    model = Review
+    form_class = UpdateReviewForm
+    template_name = "home_folder/review_db.html"
+    success_url = reverse_lazy('review_dashboard')
+
+    def get_object(self):
+        pk = self.kwargs.get("pk")
+        return get_object_or_404(Review, pk=pk)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['reviews'] = self.get_object()
+        return context
+
+class ReviewDeleteDB(DeleteView):
+    model=Review
+    template_name="home_folder/delete.html"
+    success_url= reverse_lazy('dashboard')
+
+
+
+class BlogsViewDB(ListView):
+    model= BlogDetails
+    template_name='home_folder/blogs_db.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["blogs"] = BlogDetails.objects.all()
+        context['form']=  BlogUpdateForm
+        return context
+    
+class BlogUpdateViewDB(UpdateView):
+    model= BlogDetails
+    form_class= BlogUpdateForm
+    template_name= 'home_folder/blogs_db.html'
+    success_url= reverse_lazy('blog_dashboard')
+
+    def get_object(self):
+        pk = self.kwargs.get("pk")
+        return get_object_or_404(BlogDetails, pk=pk)
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['blogs'] = self.get_object()
+        return context
+
+class BlogDeleteDB(DeleteView):
+    model=BlogDetails
+    template_name="home_folder/delete.html"
+    success_url= reverse_lazy('blog_dashboard')
+
+    
 
 
 
