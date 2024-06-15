bd565ed6f0609a57721b76cf4a4420920a7d14b1
Ujjwal Puri <ujjwalpuri221@gmail.com>
2024-06-16 00:10:16 +0545

    forms for Review, blogs, cartitems

--- mysite/customadmin/forms.py
+++ mysite/customadmin/forms.py
@@ -1,8 +1,8 @@
 from django import forms 
 from django.contrib.auth.models import User, AbstractUser
 from django.contrib.auth.forms import UserCreationForm
-from menu.models import CartItem
-
+from menu.models import *
+from home.models import *
 class UserRegistrationForm( UserCreationForm):
     model= User, AbstractUser
     fields= '__all__'
@@ -16,5 +16,19 @@ class UpdateMessageForm(forms.ModelForm):
         labels= {'message': 'Enter a Message With the Decision'}
 
 
+class UpdateReviewForm(forms.ModelForm):
+    description = forms.CharField(required=False, widget=forms.Textarea())
+    class Meta:
+        model= Review
+        fields={'is_shown','description'}
+        labels={'is_shown':'Do you want to show the review?', 'description':'Make changes in the description:'}
+
+
+class BlogUpdateForm(forms.ModelForm):
+    description= forms.CharField(widget=forms.Textarea(), max_length=250, required=False)    
+    class Meta:
+        model= BlogDetails
+        fields={'title','description', 'header_image', 'is_approved'}
+        label={'title':'Enter your title:', 'description':'Enter your description:', 'header_image':'Enter a suitable image:', 'is_approved':'Do you want the blog to appear?'}
 
 
