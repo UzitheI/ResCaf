fcd7d78f25e6e64da5d5a9b1e2e1500061dca7d3
UzitheI <ujjwalpuri221@gmail.com>
2024-06-18 17:48:30 +0545

    stars dropdown

--- mysite/home/forms.py
+++ mysite/home/forms.py
@@ -1,5 +1,9 @@
 from django import forms 
 from .models import BookATable, Review, Table, BlogDetails
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+from cked.widgets import CKEditorWidget
+from django.contrib.admin.widgets import AdminRadioSelect
+from .models import Review, RATING_CHOICES
 
 # class OrderForm(forms.ModelForm):
     
@@ -16,11 +20,12 @@ class BookingForm(forms.ModelForm):
     class Meta:
         model=BookATable
         fields=['name','customer_email','numberOfPeople','table']
-        table = forms.ModelChoiceField(
-        queryset=Table.objects.all(),
-        empty_label="Check reservation for Table description",
-        label="Table"
-    )
+        widgets={
+            'name':forms.TextInput(attrs={'class':'form-control'}),
+            'customer_email':forms.TextInput(attrs={'class':'form-control'}),
+            'numberOfPeople':forms.TextInput(attrs={'class':'form-control'}),
+            'table':forms.Select(attrs={'class':'form-control'}),
+        }
 
 class BlogForm(forms.ModelForm):
     class Meta:
@@ -30,6 +35,17 @@ class BlogForm(forms.ModelForm):
 
     
 class ReviewForm(forms.ModelForm):
+    description = forms.CharField(widget=CKEditorWidget())
+    stars = forms.ChoiceField(
+        choices=RATING_CHOICES,
+        widget=forms.Select(attrs={'class': 'form-control'}),
+        initial=5.0,
+    )
+
     class Meta:
-        model=Review
-        fields=['dish', 'stars' , 'description']
+        model = Review
+        fields = ['dish', 'stars', 'description']
+        widgets = {
+            'dish': forms.Select(attrs={'class': 'form-control'}),
+            'description': forms.Textarea(attrs={'class': 'form-control'}),
+        }
