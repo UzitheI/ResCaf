1ba80345cf0ed03ed0d4d13b8a0cda13c933e1a6
UzitheI <ujjwalpuri221@gmail.com>
2024-06-18 17:47:29 +0545

    views for dashboard authentication

--- mysite/customadmin/views.py
+++ mysite/customadmin/views.py
@@ -1,8 +1,7 @@
 
-from django.http.response import HttpResponse as HttpResponse
-from django.shortcuts import render
-from django.urls import reverse 
+
 from django.contrib.messages.views import SuccessMessageMixin
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import *
 from django.contrib.auth.views import LoginView
 from django.urls import reverse_lazy
@@ -11,6 +10,10 @@ from menu.models import *
 from home.models import *
 from customadmin.forms import *
 from django.shortcuts import get_object_or_404
+from django.contrib import messages
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+
 # Create your views here.
 class RegisterView(SuccessMessageMixin, CreateView):
     template_name= 'home_folder/register.html'
@@ -136,11 +139,49 @@ class BlogDeleteDB(DeleteView):
     model=BlogDetails
     template_name="home_folder/delete.html"
     success_url= reverse_lazy('blog_dashboard')
+    
+class AdminRegisterView(SuccessMessageMixin, CreateView):
+    template_name= 'home_folder/auth-register.html'
+    success_url= reverse_lazy('admin_login')
+    form_class= AdminRegisterForm
+    
+    success_message='Your Profile was Created Successfully'
+    
+    def form_valid(self, form):
+        # Save the form instance without committing to the database yet
+        user = form.save(commit=False)
+        # Set the user as a superuser
+        user.is_staff = True  # Make the user a staff member as well
+        # Save the user instance to the database
+        user.save()
+        return super().form_valid(form)
 
     
 
 
 
+class AdminLoginView(LoginView, LoginRequiredMixin):
+    template_name = "home_folder/auth-login.html"
+    success_url= reverse_lazy('dashboard')
+    
+    
+    def get_context_data(self, **kwargs):
+        context=super().get_context_data(**kwargs)
+        context['user']= self.request.user
+        return context
+    
+    def form_valid(self, form):
+        user = form.get_user()
+        if user.is_superuser or user.is_staff:
+            return super().form_valid(form)
+        else:
+            logout(self.request)
+            return redirect(reverse_lazy('admin_login'))
+        
+    def get_success_url(self):
+        return self.success_url
+    
+    
 
 
     
