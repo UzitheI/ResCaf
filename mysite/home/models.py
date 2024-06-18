dc17abfeb644ad5469a5fbb33406804f9b0126aa
UzitheI <ujjwalpuri221@gmail.com>
2024-06-18 17:48:48 +0545

    stars dropdown

--- mysite/home/models.py
+++ mysite/home/models.py
@@ -3,13 +3,14 @@ from django.contrib.auth.models import User
 from django.urls import reverse
 from django.utils import timezone
 from menu.models import Dish
+from cked.fields import RichTextField
 
 
 
 class Table(models.Model):
     table_number=models.IntegerField()
     table_price=models.FloatField()
-    table_description= models.TextField()
+    table_description= RichTextField()
     is_available=models.BooleanField(default=True)
 
     def __str__(self):
@@ -30,7 +31,7 @@ class BookATable(models.Model):
 class ChefDetails(models.Model):
     name=models.CharField(max_length=244)
     position=models.CharField(max_length=244)
-    description=models.CharField(max_length=244)
+    description=RichTextField()
     image=models.CharField(max_length=244)
     facebook=models.CharField(max_length=244)
     twitter=models.CharField(max_length=244)
@@ -48,7 +49,7 @@ class ChefDetails(models.Model):
 
 class BlogDetails(models.Model):
     title=models.CharField(max_length=224)
-    description=models.CharField(max_length=224)
+    description=RichTextField()
     header_image=models.CharField(max_length=224)
     writer_name=models.ForeignKey(User,on_delete=models.CASCADE, default=None)
     date=models.DateField(default=timezone.now)
@@ -57,12 +58,20 @@ class BlogDetails(models.Model):
     def __str__(self):
         return str(self.title)
 
+
+RATING_CHOICES = [
+    (1.0, '1 Star'),
+    (2.0, '2 Stars'),
+    (3.0, '3 Stars'),
+    (4.0, '4 Stars'),
+    (5.0, '5 Stars'),
+]
 class Review(models.Model):
     dish=models.ForeignKey(Dish,on_delete=models.CASCADE,default=None,null=True)
     user=models.ForeignKey(User,on_delete=models.CASCADE, default=None, null=True)
     date_time=models.DateTimeField(default=timezone.now)
-    stars=models.FloatField()
-    description=models.TextField()
+    stars=models.FloatField(choices=RATING_CHOICES, default=5.0)
+    description=RichTextField()
     is_shown= models.BooleanField(default=False)
 
     def __str__(self):
