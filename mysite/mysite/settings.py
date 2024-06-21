63904c9a5d1913db722fab5bb11750161268cf07
UzitheI <ujjwalpuri221@gmail.com>
2024-06-21 13:27:21 +0545

    changes

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -126,11 +126,11 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv('NAME'),
-        'USER': os.getenv('USER'),
-        'PASSWORD':os.getenv('PASSWORD'),
-        'HOST':os.getenv('HOST','localhost'),
-        'PORT':os.getenv('PORT'),
+        'NAME': os.getenv('DB_NAME'),
+        'USER': os.getenv('DB_USER'),
+        'PASSWORD':os.getenv('DB_PASSWORD'),
+        'HOST':os.getenv('DB_HOST','localhost'),
+        'PORT':os.getenv('DB_PORT'),
     }
 }
 
