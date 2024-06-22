7377b703ffa310469316428b7f2d0176f319266e
UzitheI <ujjwalpuri221@gmail.com>
2024-06-22 18:30:02 +0545

    smtp configuration

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -185,3 +185,12 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGOUT_REDIRECT_URL=reverse_lazy('login')
 LOGIN_REDIRECT_URL = reverse_lazy('index')
 LOGIN_URL = 'login'
+
+
+# Email settings for Gmail
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
