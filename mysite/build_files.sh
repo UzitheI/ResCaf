45b5ccab8ba5bc612b9b4b76bf43d6d2d797f849
UzitheI <ujjwalpuri221@gmail.com>
2024-08-27 21:29:00 +0545

    removed staticfiles

--- mysite/build_files.sh
+++ mysite/build_files.sh
@@ -5,7 +5,7 @@ python3 -m venv .venv
 
 source .venv/bin/activate
 
-python3 -m pip install -r requirements.txt
-python3 manage.py collectstatic --noinput
+python -m pip install -r requirements.txt
+python manage.py collectstatic --noinput
 
 echo "BUILD END"
