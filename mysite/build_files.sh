d1461106e3068da2cd71ba188dc1aec51362fe7f
UzitheI <ujjwalpuri221@gmail.com>
2024-08-27 15:41:20 +0545

    psycopg2 binary

--- mysite/build_files.sh
+++ mysite/build_files.sh
@@ -1,2 +1,9 @@
+#!/bin/bash
+set -e
+set -x
+
+# Activate virtual environment if you're using one
+# source /path/to/your/venv/bin/activate
+
 pip install -r requirements.txt
 python manage.py collectstatic --noinput
