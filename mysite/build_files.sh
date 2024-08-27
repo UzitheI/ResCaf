727dc8e49bdb0f5ae7d6da9c49aa58975f950450
UzitheI <ujjwalpuri221@gmail.com>
2024-08-27 22:09:42 +0545

    hoping

--- mysite/build_files.sh
+++ mysite/build_files.sh
@@ -1,11 +1,16 @@
+#!/bin/bash
 
 echo "BUILD START"
 
+# Create and activate the virtual environment
 python3 -m venv .venv
-
 source .venv/bin/activate
 
+# Install dependencies
+python -m pip install --upgrade pip  # Upgrade pip to avoid potential issues
 python -m pip install -r requirements.txt
+
+# Collect static files
 python manage.py collectstatic --noinput
 
 echo "BUILD END"
--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -176,7 +176,7 @@ STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
 
-STATIC_ROOT = BASE_DIR / 'staticfiles_build'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 
 
