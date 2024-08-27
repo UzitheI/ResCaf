fc8db575ec260f4ae7fd3954b741e89eaf07f36f
UzitheI <ujjwalpuri221@gmail.com>
2024-08-27 14:00:03 +0545

    finish

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = 'django-insecure-e^ir_c+eta71blp2!*b()w^nm06aqaa$%)nmee(wef#-pugos4
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", ".vercel.app", ".now.sh"]
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
--- mysite/mysite/urls.py
+++ mysite/mysite/urls.py
@@ -31,5 +31,6 @@ urlpatterns = [
     path('cked/', include('cked.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
+
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
