c7a9b0ff741a2adfc9d839623a187863b4341403
UzitheI <ujjwalpuri221@gmail.com>
2024-08-31 01:01:27 +0545

    build files

--- mysite/build_files.sh
+++ mysite/build_files.sh
@@ -1,9 +1,18 @@
-ECHO 'START'
+#!/bin/bash
 
-python -m venv .venv
-source .venv/bin/activate
+echo "STARTING DEPLOYMENT"
 
-python -m pip install -r requirements.txt 
-python manage.py collectstatic --noinput
+# Activate the virtual environment
+echo "Activating virtual environment..."
+python -m venv .venv || exit 1
+source .venv/bin/activate || exit 1
 
-ECHO 'END'
+# Install dependencies
+echo "Installing dependencies..."
+python -m pip install -r requirements.txt || exit 1
+
+# Collect static files
+echo "Collecting static files..."
+python manage.py collectstatic --noinput || exit 1
+
+echo "DEPLOYMENT SUCCESSFUL"
